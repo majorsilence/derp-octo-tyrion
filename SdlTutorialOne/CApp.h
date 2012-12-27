8068e1829c80b7a19936c40e78b0665da0e9d8ac
Peter <peter@InspironI3Laptop.ubuntu-domain>
2012-12-27 17:41:51 -0330

    SDL Tutorial 1 code.

---
+++ SdlTutorialOne/CApp.h
@@ -0,0 +1,31 @@
+#ifndef CAPP_H_INCLUDED
+#define CAPP_H_INCLUDED
+
+#include <SDL/SDL.h>
+
+class CApp{
+
+    private:
+        bool Running;
+
+        SDL_Surface* Surf_Display;
+
+    public:
+        CApp();
+
+        int OnExecute();
+
+        bool OnInit();
+
+        void OnEvent(SDL_Event* Event);
+
+        // All Game Logic
+        void OnLoop();
+
+        void OnRender();
+
+        void OnCleanup();
+
+};
+
+#endif // CAPP_H_INCLUDED
