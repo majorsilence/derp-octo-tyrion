0ec5bbba5d3943599a3b9c2b4ddea3c79fc91461
Peter <peter@InspironI3Laptop.ubuntu-domain>
2012-12-27 22:29:45 -0330

    initial code for tutorial 3

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 derp-octo-tyrion
 ================
 
-Code that is being used to follow some online sdl tutorials
+Code that is being used to follow some online sdl tutorials that I am working through
+
+http://www.sdltutorials.com/tutorials
+
+
+
+
+
+
---
+++ SdlTutorialThree/CApp.h
@@ -0,0 +1,34 @@
+#ifndef CAPP_H_INCLUDED
+#define CAPP_H_INCLUDED
+
+#include <SDL/SDL.h>
+#include "CSurface.h"
+
+class CApp{
+
+    private:
+        bool Running;
+
+        SDL_Surface* Surf_Display;
+
+        SDL_Surface* Surf_Test;
+
+    public:
+        CApp();
+
+        int OnExecute();
+
+        bool OnInit();
+
+        void OnEvent(SDL_Event* Event);
+
+        // All Game Logic
+        void OnLoop();
+
+        void OnRender();
+
+        void OnCleanup();
+
+};
+
+#endif // CAPP_H_INCLUDED
