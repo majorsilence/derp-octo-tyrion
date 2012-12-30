e26a0986532149298f09c75537ca572d2f14534e
Peter <peter@InspironI3Laptop.ubuntu-domain>
2012-12-30 11:22:55 -0330

    Redraw black background before each entity is redrawn.

--- SdlTutorialSix/CApp_OnRender.cpp
+++ SdlTutorialSix/CApp_OnRender.cpp
@@ -3,6 +3,9 @@
 void CApp::OnRender(){
 
 
+    // Fill the screen black before drawing other surfaces on it
+    SDL_FillRect( Surf_Display, &Surf_Display->clip_rect, SDL_MapRGB( Surf_Display->format, 0, 0, 0 ));
+
     for (int i=0; i<CEntity::EntityList.size(); i++){
 
         if(!CEntity::EntityList[i]){
